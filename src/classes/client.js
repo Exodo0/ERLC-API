876da22d399ed4d8e590768c2c1e96e298474cde
Egologics <69496221+Exodo0@users.noreply.github.com>
2025-02-05 17:16:20 -0600

    📝Fix

--- src/classes/client.js
+++ src/classes/client.js
@@ -1,36 +1,56 @@
-const erlc = require('../erlc.js')
-const assert = require('../functions/assert.js')
-
-/**
- * @typedef {Object} ClientConfig
- * @property {string} globalToken - Your ER:LC global API token
- */
-
-/**
- * Creates an authorised ER:LC client for requests
- * @class
- * @param {ClientConfig} options - Client Options
- */
+const assert = require('../functions/assert.js');
 
 class Client {
+  constructor(options) {
+    assert(typeof options === 'object', `Syntax error: object expected for "options", received ${typeof options}`);
+    assert(options.globalToken, 'Global token is required');
+    this.options = { ...options };
+    this.config = this.config.bind(this);
+  }
 
-    /**
-     * @constructor
-     * @param {ClientConfig} options - Client Options
-     */
-    constructor(options) {
-        assert(typeof options === 'object', `Syntax error: object expected for "options", received ${typeof options}`);
-        this.options = { ...options };
-      }
-    
-     /**
-     * Updates and returns the client configurationg
-     * @returns {ClientConfig} The client configuration.
-     */
-    config() {
-        erlc.config = this.options
-        return erlc.config
-    }
+  config() {
+    return this.options;
+  }
+
+  async getServer(serverToken) {
+    return require('../functions/server/getServer.js')(serverToken);
+  }
+
+  async getPlayers(serverToken) {
+    return require('../functions/server/getPlayers.js')(serverToken);
+  }
+
+  async getJoinLogs(serverToken) {
+    return require('../functions/server/getJoinLogs.js')(serverToken);
+  }
+
+  async getKillLogs(serverToken) {
+    return require('../functions/server/getKillLogs.js')(serverToken);
+  }
+
+  async getCommandLogs(serverToken) {
+    return require('../functions/server/getCommandLogs.js')(serverToken);
+  }
+
+  async getModcallLogs(serverToken) {
+    return require('../functions/server/getModcallLogs.js')(serverToken);
+  }
+
+  async getBans(serverToken) {
+    return require('../functions/server/getBans.js')(serverToken);
+  }
+
+  async getVehicles(serverToken) {
+    return require('../functions/server/getVehicles.js')(serverToken);
+  }
+
+  async getQueue(serverToken) {
+    return require('../functions/server/getQueue.js')(serverToken);
+  }
+
+  async runCommand(serverToken, command) {
+    return require('../functions/server/runCommand.js')(serverToken, command);
+  }
 }
 
-module.exports = Client
+module.exports = Client;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-import { Client } from './client';
-import * as Types from './types';
-import * as Errors from './errors';
-
-const ERLC = {
-  Client,
-  ...Types,
-  ...Errors,
-};
-
-export default ERLC;
-export { Client, Types, Errors };
+const ERLC = require('./src/erlc');
+module.exports = ERLC;
