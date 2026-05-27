1542d2b2564fe88e2c7cb7542991e135f6395c73
Exodo0 <69496221+Exodo0@users.noreply.github.com>
2026-05-27 00:31:04 -0600

    Update Vanity URL

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-exports.Vanity = "https://policeroleplay.community/join?code="
+exports.Vanity = "https://erlc.gg/join/"
 exports.API_ORIGIN = "https://api.erlc.gg"
 exports.API_VERSION = "v2"
 exports.BASEURL = `${exports.API_ORIGIN}/${exports.API_VERSION}`
